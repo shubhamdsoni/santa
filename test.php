01b05a59ba934c1cbe45249ef5f59c4adcfdd046
Shubham Soni <shubhamsoni@gmail.com>
2020-12-15 10:25:47 +0530

    Create test.php

---
+++ test.php
@@ -0,0 +1,66 @@
+<html>
+<body>
+<?php
+echo(rand(10,100));
+?>
+
+<p> The holidays can be pretty hectic. They're full of people shopping for gifts, baking dozens of cookies, decorating trees, and wrapping hundreds of presents. However, when you spend most of your time at work, it's sometimes difficult to get into the holiday spirit. </p>
+
+<p> Adding holiday festivities around the office, like Secret Santa, might make the workplace a little merrier.  So we decided to play the Secret Santa to end this year 2020 with some fun. </p>
+
+<h2><span style="color: #3095e3;"> # 1 The Do's and Don'ts</span></h2>
+
+<p>For those who don't know, Secret Santa is a gift exchange done in the spirit of old St. Nick. The idea of the game is to play Santa himself, but shhh, you can't tell anyone who you have!</p>
+<p>Secret Santa can be a lot of fun and is an excellent way to get the us together for the holiday season. However, when it’s not executed properly, the experience can be a total downer for everyone involved.</p>
+
+<p>These directions and rules will put you on track for a smooth, gift-giving event!</p>
+
+<p><strong>The Do's:</strong></p>
+
+<ul>
+    <li><strong>Spending Limit is Minimum Rs 200/- and Maximum is Rs 500/.</strong></li>
+    <li><strong>Be unique and creative.</strong></li>
+    <li><strong>Participate.</strong></li>
+</ul>
+
+<p><strong>The Don'ts:</strong></p>
+
+<ul>
+    <li><strong>Tell anyone who you have - It’s called Secret Santa for a reason!</strong></li>
+    <li><strong>Go over budget - How upset would you be if your santa spent Rs 100 on your gift when you spent Rs 500 on his/her gift?</strong></li>
+    <li><strong>Be passive-aggressive with your gift.</strong></li>
+</ul>
+
+
+<h2><span style="color: #3095e3;"> # 2 Gifts Rule</span></h2>
+
+<p>Setting ground rules upfront will ensure everyone receives a gift within the price limit and nobody's feelings get hurt. After all, Secret Santa is about bringing joy to the office, not leaving people feeling blue.</p>
+
+<p><strong>Lets decide the Rules, There will be two gifts</strong></p>
+
+<ul>
+    <li><strong>A Book that will describe the personality of that person.</strong></li>
+    <li><strong>A Secret Santa Gift, that will match the Do's ans Don'ts category.</strong></li>
+</ul>
+
+<h2><span style="color: #3095e3;"> # 3 Gifts to Avoid</span></h2>
+
+<p>Buying presents for people, even when you know them personally, can be difficult and frustrating. The endless supply of options and the desire to make sure you get the best gift are what make the task of giving so tricky. Narrow down your options with this list of gifts you should never buy someone, ever.</p>
+
+<ul>
+    <li><strong>Gift cards or money</strong> - While gift cards are great for birthday or appreciation gifts, they scream "lazy" for Christmas. Nothing says, "I don't know anything about you" like money.</li>
+    <li><strong>Used or opened items</strong> - When it comes to gifting presents, it's the thought that counts; however, regifting items shows you put zero thought into the gift at all. Going this route will make you look cheap and tacky.</li>
+    <li><strong>Controversial gifts</strong> - Items like underwear, diet pills, toiletries, or gag gifts are not appropriate and should never be given to your co-worker, especially at a work event. Presents that poke fun at political issues, religious beliefs, or gender identity is a no-go as well.</li>
+    <li><strong>Food</strong> - Cookies, candy, and brownies are sweet and delicious, but they're not proper gifts. Although intentions might be good with this one, most people might not want treats in place of an actual present. Also, you never know who has allergies.</li>
+    <li><strong>Clothes</strong> - I know some people might appreciate clothes as a present, but if you're anything like me, it's the absolute worst. We're all adults here and unless someone asked for a specific item, we don't need other people picking out our clothes for us.</li>
+</ul>
+
+<h2><span style="color: #3095e3;"> # 4 Have a Blast</span></h2>
+
+<p>The important thing to remember is that Secret Santa isn't just about the presents. It’s about destressing at the office, spending quality time with your co-workers, and rounding out the year on a positive note. After all, everyone has worked hard and deserves a teeny, tiny break, including the seniors!</p>
+
+<p>So Finally it's time to reveil the Your Santa</p>
+
+</body>
+</html>
+
